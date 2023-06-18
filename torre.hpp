d9fc978557c32c7948f9425c32657b3fcf81f1f1
AlejandroMoyanoCrepillo <mixalejandro04@gmail.com>
2023-06-18 19:10:54 +0200

    Comentado función mover();
    Se ha explicado detalladamente el comportamiento de la función.

--- torre.hpp
+++ torre.hpp
@@ -22,6 +22,20 @@ namespace Moyanos
         // Comprueba si es posible el paso de la pieza del origen al destino
         // a traves del parametro ok, y en caso de serlo devuelve un vector
         // con las posisciones por las cuales pasaria
+        //
+        // Parametros de entrada:
+        // origen --> posicion actual de la pieza. Ejemplo([0,0])
+        // destino --> posicion final de la pieza. Ejemplo([4,0])
+        //
+        // Parametros de salida:
+        // paso --> Lugares por los que pasa la pieza en el tablero.
+        // ok --> Dictamina si es posible el movimiento en
+        //        caso de no haber piezas por medio
+
+        // Ejemplo :
+        // torre.mover([0,0] , [4,0] , paso , ok)
+        // paso = [1,0] , [2,0] , [3,0]
+        // ok = True
         virtual void mover(const std::array<unsigned, 2> &origen,
                            const std::array<unsigned, 2> &destino,
                            const std::vector<std::array<unsigned, 2>> &paso,
