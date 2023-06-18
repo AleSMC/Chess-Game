4b200170aedd7023f0c3cd535ceaae36b03f7528
AlejandroMoyanoCrepillo <mixalejandro04@gmail.com>
2023-06-18 18:52:35 +0200

    Create class Torre

---
+++ torre.hpp
@@ -0,0 +1,16 @@
+#ifndef __TORRE_HPP__
+#define __TORRE_HPP__
+
+#include "pieza.hpp"
+
+namespace Moyanos
+{
+    class Torre : public Pieza
+    {
+    private:
+    protected:
+    public:
+    };
+}
+
+#endif
