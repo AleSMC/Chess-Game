c556c4fb54c6585c94e452dd2c23cd29481165aa
AlejandroMoyanoCrepillo <mixalejandro04@gmail.com>
2023-06-18 18:24:08 +0200

    Definir clase Pieza

--- pieza.hpp
+++ pieza.hpp
@@ -6,7 +6,29 @@ namespace Moyanos
     class Pieza
     {
     private:
+        // La variable hace referencia al color de la pieza
+        // Negro = 1
+        // Blanco = 0
+        unsigned color;
+
+    protected:
+        Pieza(const Pieza &other);
+
     public:
+        // Constructor especifico
+        Pieza(unsigned c);
+
+        // Destructor
+        virtual ~Pieza();
+
+        // Devuelve el color de la pieza
+        virtual unsigned getColor() const;
+
+        // Devuelvo una copia del objeto pieza
+        virtual Pieza *clonar() const;
+
+        // Desactivado operator =
+        Pieza &operator=(const Pieza &) = delete;
     };
 }
 
